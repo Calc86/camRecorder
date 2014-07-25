c9c49914db6ef18d2e35e9dafb1f40a18d5d69ab
calc <calc@list.ru>
2014-07-25 13:50:58 +0400

    out can be null

--- src/com/net/utils/OutputStreamHolder.java
+++ src/com/net/utils/OutputStreamHolder.java
@@ -4,35 +4,45 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 public class OutputStreamHolder extends OutputStream {
-    volatile private OutputStream out;
+    volatile private OutputStream out = null;
+
+    public OutputStreamHolder() {
+        this(null);
+    }
 
     public OutputStreamHolder(final OutputStream out) {
-        this.out = out;
+        if(this.out != null)
+            this.out = out;
     }
 
     @Override
     public void write(int b) throws IOException {
-        out.write(b);
+        if(this.out != null)
+            out.write(b);
     }
 
     @Override
     public void write(byte[] b) throws IOException {
-        out.write(b);
+        if(this.out != null)
+            out.write(b);
     }
 
     @Override
     public void write(byte[] b, int off, int len) throws IOException {
-        out.write(b, off, len);
+        if(this.out != null)
+            out.write(b, off, len);
     }
 
     @Override
     public void flush() throws IOException {
-        out.flush();
+        if(this.out != null)
+            out.flush();
     }
 
     @Override
     public void close() throws IOException {
-        out.close();
+        if(this.out != null)
+            out.close();
     }
 
         /*public void setOut(OutputStream out) {
@@ -40,8 +50,8 @@ public class OutputStreamHolder extends OutputStream {
         }*/
 
     public synchronized void change(final OutputStream out) throws IOException {
-        this.out.flush();
-        this.out.close();
+        flush();
+        close();
         this.out = out;
     }
 }

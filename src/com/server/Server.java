20b1f267599562a7845577df3f5f3402b87bd24f
calc <calc@list.ru>
2014-07-31 16:03:08 +0400

    logging

--- src/com/server/Server.java
+++ src/com/server/Server.java
@@ -7,8 +7,8 @@ import com.net.rtp.H264RTP;
 import com.net.rtsp.Reply;
 import com.net.rtsp.Rtsp;
 import com.net.rtsp.SDP;
-import com.net.utils.OutputStreamHolder;
 import org.apache.commons.lang3.NotImplementedException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 
 import java.io.*;
 import java.net.MalformedURLException;
@@ -16,6 +16,7 @@ import java.net.URL;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -50,9 +51,16 @@ public class Server {
     }
 
     public void start() throws NotImplementedException {
+        log.info("start server");
+        log.info(String.format("settings \n ffmpeg: %s\n vlc: %s\n seconds: %d\n",
+                Settings.getInstance().getFfmpegPath(),
+                Settings.getInstance().getVlcPath(),
+                Settings.getInstance().getSeconds()
+                ));
+
         threads.clear();
         if(!isStop()) {
-            System.err.println("server already started");
+            log.warning("server already started");
             return;
         }
 
@@ -74,18 +82,21 @@ public class Server {
             }
 
         } catch (SQLException e) {
-            e.printStackTrace();
+            log.log(Level.SEVERE, ExceptionUtils.getStackTrace(e));
+            //e.printStackTrace();
         }
     }
 
     public void stop(){
+        log.info("stop");
         stop = true;
 
         for(Thread t : threads){
             try {
                 t.join();
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                log.log(Level.SEVERE, ExceptionUtils.getStackTrace(e));
+                //e.printStackTrace();
             }
         }
     }
@@ -108,8 +119,6 @@ public class Server {
             HTTPReceiver.play();
 
             while (!stop){
-                //Thread.sleep(LENGTH_OF_RECORD_IN_SECONDS * 1000);
-                //Thread.sleep(Settings.getInstance().getSeconds() * 1000);
                 int wait = Settings.getInstance().getSeconds();
                 while(wait > 0 && !stop){
                     Thread.sleep(1000);
@@ -145,7 +154,7 @@ public class Server {
         final Rtsp rtsp = new Rtsp();
 
         try {
-            rtsp.setDebug(Settings.getInstance().isDebug());
+            //rtsp.setDebug(Settings.getInstance().isDebug());
             rtsp.connect(cam.getUrl());
 
             rtsp.options();
@@ -153,9 +162,9 @@ public class Server {
 
             //добавить проверки
             String video = sdp.getMediaByType(SDP.MediaType.video).get(0).getAttribute(SDP.AttributeName.control).get(0);
-            System.out.println("video:" + video);
+            if(log.isLoggable(Level.FINE)) log.fine("video:" + video);
             String audio = sdp.getMediaByType(SDP.MediaType.audio).get(0).getAttribute(SDP.AttributeName.control).get(0);
-            System.out.println("video:" + audio);
+            if(log.isLoggable(Level.FINE)) log.fine("audio:" + audio);
 
             SDP.FMTP fmtp = getFmtp(sdp);
 
@@ -164,18 +173,17 @@ public class Server {
             boolean interleaved = true;
 
             //rtsp.setup(video, 0, 1, true, "");
-            rtsp.setup(video, 0, 1, interleaved, "");
+            rtsp.setup(video, interleaved);
             Reply reply = rtsp.getLastReply();
             String session = reply.getSession();
             if(reply.getCode() == 403){
-                System.err.println("Non interleaved mode not supported");
+                log.warning("Non interleaved mode not supported");
                 interleaved = !interleaved;
-                rtsp.setup(video, 0, 1, interleaved, session);
+                rtsp.setup(video, interleaved);
             }
 
-            System.out.println("session: " + session);
-            //rtsp.setup(audio, 2, 3, true, session);
-            rtsp.setup(audio, 2, 3, interleaved, session);
+            if(log.isLoggable(Level.FINE)) log.fine("session: " + session);
+            rtsp.setup(audio, interleaved);
 
             ByteArrayOutputStream fmtpBuffer = new ByteArrayOutputStream();
             if(fmtp != null){
@@ -192,18 +200,15 @@ public class Server {
             //save only video
             outs[0] = rotator;
 
-            rtsp.play(session, outs);
+            rtsp.play(outs);
 
-            int i = 1;
             while (!stop){
-                //Thread.sleep(LENGTH_OF_RECORD_IN_SECONDS * 1000);
                 int wait = Settings.getInstance().getSeconds();
                 while(wait > 0 && !stop){
                     Thread.sleep(1000);
                     wait--;
                 }
 
-                //Thread.sleep(Settings.getInstance().getSeconds() * 1000);
                 if(!stop){
                     if(fmtp != null) rotator.rotate(fmtpBuffer.toByteArray());
                     else rotator.rotate();
@@ -212,16 +217,16 @@ public class Server {
             try {
                 rtsp.stop();
             } catch (Exception e) {
-                e.printStackTrace();
+                log.log(Level.SEVERE, ExceptionUtils.getStackTrace(e));
             } finally {
                 rotator.close();
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            log.log(Level.SEVERE, ExceptionUtils.getStackTrace(e));
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            log.log(Level.SEVERE, ExceptionUtils.getStackTrace(e));
         } catch (SQLException e) {
-            e.printStackTrace();
+            log.log(Level.SEVERE, ExceptionUtils.getStackTrace(e));
         } finally {
             stop = true;
         }

c3885aad2f0f73c1bf77d6459ebeea09cc04fedb
calc <calc@list.ru>
2014-08-04 18:13:40 +0400

    concat video

--- src/com/video/FFmpeg.java
+++ src/com/video/FFmpeg.java
@@ -4,10 +4,8 @@ import com.model.Archive;
 import com.model.Settings;
 import com.server.Server;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -20,6 +18,73 @@ public class FFmpeg {
     public void deleteOld(String archive){
         File f = new File(Settings.getInstance().getFullTmpPath() + archive);
         f.delete();
+    }
+
+    /**
+     * https://trac.ffmpeg.org/wiki/How%20to%20concatenate%20(join,%20merge)%20media%20files
+     * @param list of Archives
+     * @throws IOException
+     */
+    public void createConcatFile(List<Archive> list) throws IOException {
+        /*
+        # this is a comment
+        file '/path/to/file1'
+        file '/path/to/file2'
+        file '/path/to/file3'
+         */
+
+        File concat = new File("concat.txt");
+
+        if(concat.exists())
+            concat.delete();
+
+        concat.createNewFile();
+
+        FileWriter writer = new FileWriter(concat);
+        for(Archive archive : list){
+            String path = Settings.getInstance().getFullRecPath() + archive.getFileName() + ".mp4";
+            File file = new File(path);
+            if(!file.exists()){
+                log.warning(String.format("file %s not exist", path));
+                continue;
+            }
+            String line = String.format("file '%s'\r\n", path);
+            writer.write(line);
+        }
+
+        writer.flush();
+        writer.close();
+    }
+
+    public void concat(){
+        String command = String.format("%s -y -f concat -i concat.txt -codec copy concat.mp4",
+                Settings.getInstance().getFfmpegPath());
+        log.info(command);
+
+        try {
+            Runtime rt = Runtime.getRuntime();
+            Process pr = rt.exec(command);
+
+            /*InputStream stdin = pr.getInputStream();
+            InputStreamReader isr = new InputStreamReader(stdin);
+            BufferedReader br = new BufferedReader(isr);
+
+            String line = null;
+            System.out.println("<OUTPUT>");
+
+            while ( (line = br.readLine()) != null)
+                System.out.println(line);
+
+            System.out.println("</OUTPUT>");*/
+
+            int exitVal = pr.waitFor();
+            if(exitVal != 0)
+                log.warning("FFmpeg " + command + " exited with error code " + exitVal);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void move(String archive){

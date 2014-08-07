8ff785765cc1fcff56ac12e472461df8cfaa011f
calc <calc@list.ru>
2014-08-07 15:47:24 +0400

    Класс для тестов

---
+++ src/com/net/utils/RandomSleep.java
@@ -0,0 +1,17 @@
+package com.net.utils;
+
+/**
+ * Created by calc on 07.08.14.
+ *
+ */
+public class RandomSleep {
+    public static void sleep(int min, int max){
+        int time = (int)(Math.random() * (max-min) + min);
+        System.out.println("Sleep: " + time);
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
